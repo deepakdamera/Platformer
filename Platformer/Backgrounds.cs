dbaed554522dc7cdbefa1cc5cb8c1c781cfe7cea
deepakdamera <deepakdamera@COLLEGEMAN>
2019-03-31 01:24:44 -0500

    scrollin

---
+++ Platformer/Backgrounds.cs
@@ -0,0 +1,21 @@
+﻿
+using Microsoft.Xna.Framework.Game;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
+
+namespace Platformer
+{
+    class Backgrounds
+    {
+        Texture2D texture;
+         Rectangle rectangle;
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw
+        }
+
+
+    }
+}
