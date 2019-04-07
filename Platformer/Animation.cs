04171c17a67684445a64561597d592f14bd47e99
deepakdamera <deepakdamera@COLLEGEMAN>
2019-04-07 00:34:40 -0500

    new classes for animation

---
+++ Platformer/Animation.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Platformer
+{
+    class Animation
+    {
+
+        public int CurrFrame { get; set; }
+        public int FrameCount { get; private set; }
+        public int FrameHeight { get { return Texture.Height; } }
+        public int FrameSpeed { get; set; }
+        public int FrameWidth { get { return Texture.Width / FrameCount; } } 
+        public bool isLooping { get; set; }
+        public Animation( Texture2D texture, int frameCount)
+        {
+            Texture = texture;
+            FrameCount = frameCount;
+            isLooping = true;
+            FrameSpeed = 0.2f;
+        }
+
+    }
+
+
+}
---
+++ Platformer/AnimationManager.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class AnimationManager
+{
+    private Animation _animation;
+    private float timer;
+    public Vector2 Position { get; set; }
+
+    public AnimationManager(Animation animation)
+    {
+        if (_animation == animation)
+            return;
+
+        _animation = animation;
+        _animation.CurrFrame = 0;
+        _timer = 0;
+    }
+
+    public void Play(Animation animation)
+    {
+        if (_animation == animation)
+            return;
+        _animation = animation;
+        animation.CurrFrame = 0;
+        timer = 0;
+    }
+	private Animation()
+	{
+	}
+
+    public void Stop()
+    {
+        timer = 0;
+        _animation.CurrFrame = 0;
+    }
+}
